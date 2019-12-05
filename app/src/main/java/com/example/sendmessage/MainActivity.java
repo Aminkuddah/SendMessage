d54f519d4b6179814c8862438483e340cb21c86e
amin kuddah <aminkuddah@gmail.com>
2019-12-05 09:12:56 +0700

    nambah data ke firebase

--- app/src/main/java/com/example/sendmessage/MainActivity.java
+++ app/src/main/java/com/example/sendmessage/MainActivity.java
@@ -1,18 +1,21 @@
 package com.example.sendmessage;
 
-import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.content.DialogInterface;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.EditText;
+
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
 
 public class MainActivity extends AppCompatActivity {
     private EditText namaInput;
     private EditText alamatInput;
     private EditText hpInput;
     String nis, nama, alamat, hp, keterangan;
+
+    private DatabaseReference mDatabase;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -23,36 +26,50 @@ public class MainActivity extends AppCompatActivity {
         hpInput= findViewById(R.id.hp);
     }
 
-    public void alertButton(View view) {
+    public void insertButton(View view) {
+
+        mDatabase = FirebaseDatabase.getInstance().getReference();
+
         nama= namaInput.getText().toString();
         alamat= alamatInput.getText().toString();
         hp = hpInput.getText().toString();
 
-        AlertDialog.Builder alertDialogBuilder = new AlertDialog.Builder(
-                this);
+        mDatabase.child("data").child("nama").setValue(nama);
+        mDatabase.child("data").child("alamat").setValue(alamat);
+        mDatabase.child("data").child("hp").setValue(hp);
 
-        // set title dialog
-        alertDialogBuilder.setTitle("Data siswa");
-
-        // set pesan dari dialog
-        alertDialogBuilder
-                .setMessage("Nama : " + nama + "\nAlamat : "+ alamat +"\nhandphone : "+hp)
-                .setCancelable(false)
-                .setPositiveButton("Ya",new DialogInterface.OnClickListener() {
-                    public void onClick(DialogInterface dialog,int id) {
-                        MainActivity.this.finish();
-                    }
-                })
-                .setNegativeButton("Tidak",new DialogInterface.OnClickListener() {
-                    public void onClick(DialogInterface dialog, int id) {
-                        dialog.cancel();
-                    }
-                });
-
-        // membuat alert dialog dari builder
-        AlertDialog alertDialog = alertDialogBuilder.create();
-
-        // menampilkan alert dialog
-        alertDialog.show();
     }
+
+//    public void alertButton(View view) {
+//        nama= namaInput.getText().toString();
+//        alamat= alamatInput.getText().toString();
+//        hp = hpInput.getText().toString();
+//
+//        AlertDialog.Builder alertDialogBuilder = new AlertDialog.Builder(
+//                this);
+//
+//        // set title dialog
+//        alertDialogBuilder.setTitle("Data siswa");
+//
+//        // set pesan dari dialog
+//        alertDialogBuilder
+//                .setMessage("Nama : " + nama + "\nAlamat : "+ alamat +"\nhandphone : "+hp)
+//                .setCancelable(false)
+//                .setPositiveButton("Ya",new DialogInterface.OnClickListener() {
+//                    public void onClick(DialogInterface dialog,int id) {
+//                        MainActivity.this.finish();
+//                    }
+//                })
+//                .setNegativeButton("Tidak",new DialogInterface.OnClickListener() {
+//                    public void onClick(DialogInterface dialog, int id) {
+//                        dialog.cancel();
+//                    }
+//                });
+//
+//        // membuat alert dialog dari builder
+//        AlertDialog alertDialog = alertDialogBuilder.create();
+//
+//        // menampilkan alert dialog
+//        alertDialog.show();
+//    }
 }
