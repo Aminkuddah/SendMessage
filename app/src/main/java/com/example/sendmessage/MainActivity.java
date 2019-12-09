4d0b150a709b2cff514ffa6fa8c29c3b8acffc5a
panjiad <panjiawwaludi430@gmail.com>
2019-12-09 19:26:21 +0700

    fix notification

--- app/src/main/java/com/example/sendmessage/MainActivity.java
+++ app/src/main/java/com/example/sendmessage/MainActivity.java
@@ -1,7 +1,11 @@
 package com.example.sendmessage;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.NotificationCompat;
 
+import android.app.Notification;
+import android.app.NotificationManager;
+import android.content.Context;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.EditText;
@@ -14,6 +18,7 @@ public class MainActivity extends AppCompatActivity{
     private EditText alamatInput;
     private EditText hpInput;
     String nama, alamat, hp;
+    String CHANNEL_ID = "my_channel_01";
 
     private DatabaseReference mDatabase;
 
@@ -33,13 +38,23 @@ public class MainActivity extends AppCompatActivity{
         alamat= alamatInput.getText().toString();
         hp = hpInput.getText().toString();
 
+        final Notification.Builder builder = new Notification.Builder(MainActivity.this)
+                .setTicker("TickerTitle")
+                .setContentTitle("Data siswa")
+                .setContentText("Biodata siswa berhasil ditambahkan")
+                .setSmallIcon(R.drawable.ic_launcher_background);
+
         DatabaseReference newData = mDatabase.child("data").push();
         newData.child("nama").setValue(nama);
         newData.child("alamat").setValue(alamat);
         newData.child("hp").setValue(hp).addOnSuccessListener(new OnSuccessListener<Void>() {
             @Override
             public void onSuccess(Void aVoid) {
-                System.out.println("Data berhasil ditambahkan");
+//                System.out.println("Data berhasil ditambahkan");
+
+                NotificationManager nm = (NotificationManager)getSystemService(NOTIFICATION_SERVICE);
+                nm.notify(0, builder.build());
+
             }
         });
     }
