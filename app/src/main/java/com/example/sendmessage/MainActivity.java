dbdfec527d1095798274bb834111cdda9f6099d5
panjiad <panjiawwaludi430@gmail.com>
2019-12-09 18:32:33 +0700

    alert

--- app/src/main/java/com/example/sendmessage/MainActivity.java
+++ app/src/main/java/com/example/sendmessage/MainActivity.java
@@ -5,11 +5,11 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.EditText;
-
+import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity{
     private EditText namaInput;
     private EditText alamatInput;
     private EditText hpInput;
@@ -21,14 +21,13 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        mDatabase = FirebaseDatabase.getInstance().getReference();
         namaInput = findViewById(R.id.nama);
         alamatInput = findViewById(R.id.alamat);
         hpInput= findViewById(R.id.hp);
     }
 
     public void insertButton(View view) {
-
-        mDatabase = FirebaseDatabase.getInstance().getReference();
 
         nama= namaInput.getText().toString();
         alamat= alamatInput.getText().toString();
@@ -37,39 +36,11 @@ public class MainActivity extends AppCompatActivity {
         DatabaseReference newData = mDatabase.child("data").push();
         newData.child("nama").setValue(nama);
         newData.child("alamat").setValue(alamat);
-        newData.child("hp").setValue(hp);
+        newData.child("hp").setValue(hp).addOnSuccessListener(new OnSuccessListener<Void>() {
+            @Override
+            public void onSuccess(Void aVoid) {
+                System.out.println("Data berhasil ditambahkan");
+            }
+        });
     }
-
-//    public void alertButton(View view) {
-//        nama= namaInput.getText().toString();
-//        alamat= alamatInput.getText().toString();
-//        hp = hpInput.getText().toString();
-//
-//        AlertDialog.Builder alertDialogBuilder = new AlertDialog.Builder(
-//                this);
-//
-//        // set title dialog
-//        alertDialogBuilder.setTitle("Data siswa");
-//
-//        // set pesan dari dialog
-//        alertDialogBuilder
-//                .setMessage("Nama : " + nama + "\nAlamat : "+ alamat +"\nhandphone : "+hp)
-//                .setCancelable(false)
-//                .setPositiveButton("Ya",new DialogInterface.OnClickListener() {
-//                    public void onClick(DialogInterface dialog,int id) {
-//                        MainActivity.this.finish();
-//                    }
-//                })
-//                .setNegativeButton("Tidak",new DialogInterface.OnClickListener() {
-//                    public void onClick(DialogInterface dialog, int id) {
-//                        dialog.cancel();
-//                    }
-//                });
-//
-//        // membuat alert dialog dari builder
-//        AlertDialog alertDialog = alertDialogBuilder.create();
-//
-//        // menampilkan alert dialog
-//        alertDialog.show();
-//    }
 }
